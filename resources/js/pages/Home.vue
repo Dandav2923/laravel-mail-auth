e71296894c46d1895eaf9b0e788ed65363f8dff0
Dandav2923 <daniele.emili23@gmail.com>
2022-03-14 17:05:08 +0100

    scaffolding

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+      Home
+  </div>
+</template>
+
+<script>
+export default {
+    name:"home"
+}
+</script>
+
+<style>
+</style>
+© 2022 GitHub, Inc.
+Terms
